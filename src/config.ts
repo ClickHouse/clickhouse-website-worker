2687dd863415d40c81334a54954bf5b085b55e30
Chris Carlevato <chris@flickerbox.com>
2022-02-24 11:26:23 -0500

    Remove pre-launch comment

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,5 @@
 export default {
   domain: 'clickhouse.com',
-  /**
-   * origins for github and pantheon are currently the same intentionally and should
-   * remain so until the production WordPress site is launched. This will permit
-   * us to test staging first without disrupting the existing production site.
-   */
   origins: {
     github: 'content.clickhouse.com',
     pantheon: 'live-clickhouse.pantheonsite.io',
