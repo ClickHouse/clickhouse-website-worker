eeccdfec34c93f7469b3ce23f2f602f5d4cc3d1d
Mikhail f. Shiryaev <felixoid@clickhouse.com>
2022-03-14 12:39:30 +0100

    Cache redirects to big packages

--- src/packages.ts
+++ src/packages.ts
@@ -16,17 +16,7 @@ export async function handlePackagesRequest(request: Request) {
   url.hostname = domain;
   url.pathname = pathPrefix + path;
   if (path.endsWith('.deb') || path.endsWith('.rpm') || path.endsWith('.tgz')) {
-    // Return files with 7d TTL
-    const cf = {
-      cf: {
-        cacheEverything: true,
-        cacheTtlByStatus: {
-          "200-299": 7 * 86400,
-          "300-599": 5,
-        },
-      },
-    };
-    return fetch(changeUrl(request, url), cf);
+    return getRedirectedPackage(request, url, 0);
   }
 
   // For redirects we rewrite location to a proper domain
@@ -36,6 +26,28 @@ export async function handlePackagesRequest(request: Request) {
   const toReplace = domain + pathPrefix;
   if (location && location.indexOf(toReplace) >= 0) {
     response.headers.set('location', location.replace(toReplace, origin));
+  }
+  return response;
+}
+
+async function getRedirectedPackage(request: Request, url: URL, redirects: number):Promise<Response> {
+  // Jfrog put big files to S3 and redirects original requests there
+  // The 5 redirects is the maximum depth
+  let maxRedirects = 5;
+  const cf = {
+    cf: {
+      cacheEverything: true,
+      cacheTtlByStatus: {
+        // Return files with 7d TTL
+        "200-399": 7 * 86400,
+        "400-599": 5,
+      },
+    },
+  };
+  let response = await fetch(changeUrl(request, url), cf);
+  const location = response.headers.get('location');
+  if (location && redirects < maxRedirects) {
+    return getRedirectedPackage(request, new URL(location), redirects+1);
   }
   return response;
 }
