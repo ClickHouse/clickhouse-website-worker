ee8d4707d36988c46fa73cbfcf158b945f20ba6f
Alexey Milovidov <milovidov@yandex-team.ru>
2022-04-20 22:42:41 +0200

    Update config, add new source

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,24 @@
 export default {
   domain: 'clickhouse.com',
+
   origins: {
+    /// The content of this part of the website is hosted by GitHub Pages.
+    /// We only proxy it on clickhouse.com.
     github: 'content.clickhouse.com',
+    github_docs_content: 'docs-content.clickhouse.tech',
+
+    /// The content of this part of the website is hosted by external company.
     pantheon: 'live-clickhouse.pantheonsite.io',
     pantheon_staging: 'staging-clickhouse.pantheonsite.io',
   },
+
   redirects: {
     github: 'clickhouse.com',
     pantheon: 'clickhouse.com',
     pantheon_staging: 'staging.clickhouse.com',
   },
+
   codebrowser: 'clickhouse-test-reports.s3.amazonaws.com',
+
   production: true,
 };
