5028ef647693c6439738a616915ad8efda1db68d
Mikhail f. Shiryaev <felixoid@clickhouse.com>
2022-09-22 09:24:34 +0200

    Migrate packages.clickhouse.com to r2 storage

--- src/packages.ts
+++ src/packages.ts
@@ -1,18 +1,19 @@
+// The source of the file is got from:
+// + https://github.com/skymethod/denoflare/blob/v0.5.8/examples/r2-public-read-worker/worker.ts
+// License: MIT
 import { addDefaultHeaders, changeUrl } from './util';
-
-const pathPrefix = '/artifactory';
-const slash = `<!DOCTYPE html>
-<html>
-<head><title>Index of /</title></head>
-<body>
-<h1>Index of /</h1>
-<pre><a href="deb/">deb/</a>
-<a href="rpm/">rpm/</a>
-<a href="tgz/">tgz/</a></pre>
-</body></html>`
+import {
+  TEXT_HTML_UTF8,
+  computeDirectoryListingHtml,
+  computeHeaders,
+  computeObjResponse,
+  permanentRedirect,
+  temporaryRedirect,
+  tryParseR2Conditional,
+  tryParseRange,
+} from './r2';
 
 export async function handlePackagesRequest(request: Request) {
-  const domain = JFROG_DOMAIN;
   let url = new URL(request.url);
   // hostname in the request
   const hostname = url.hostname;
@@ -21,76 +22,77 @@ export async function handlePackagesRequest(request: Request) {
     url.protocol = "https:";
     return Response.redirect(url.toString(), 301);
   };
-  // Return pre-generated page for the home page
-  if (url.pathname === '/') {
-    const init = {
-      headers: {
-        'content-type': 'text/html;charset=UTF-8',
-      },
-    };
-    return new Response(slash, init);
+  const objectName = url.pathname.slice(1);
+  const method = request.method;
+  if (method != "GET" && method != "HEAD") {
+    return new Response(`Unsupported method`, {
+      status: 405
+    })
   }
+  const headers = request.headers;
+  const { pathname, searchParams } = url;
+  let key = pathname.substring(1); // strip leading slash
+  let bucket : R2Bucket = PACKAGES_BUCKET;
+  key = decodeURIComponent(key);
 
-  // Create a copy of request for writeable headers
-  request = new Request(url.toString(), request);
-  // Add auth header and prevent redirecting to UI interface
-  request.headers.set('X-JFrog-Art-Api', JFROG_ART_API_KEY);
-  request.headers.set('User-Agent', 'curl');
+  console.log(`${request.method} object ${objectName}: ${request.url}`)
+  let range = tryParseRange(headers);
+  const onlyIf = tryParseR2Conditional(headers);
+  let obj: R2Object | null = null;
+  const getOrHead: (key: string, options?: R2GetOptions) => Promise<R2Object | null> = (key, options) => {
+    console.log(`bucket.${method.toLowerCase()} ${key} ${JSON.stringify(options)}`);
+    return method === 'GET' ? (options ? bucket.get(key, options) : bucket.get(key)) : bucket.head(key);
+  };
+  obj = key === '' ? null : await getOrHead(key, { range, onlyIf });
 
-  // Generate new URL
-  const path = url.pathname;
-  url.hostname = domain;
-  url.pathname = pathPrefix + path;
-
-  // Don't follow redirects for packages, instead cache response with original names
-  if (path.endsWith('.deb') || path.endsWith('.rpm') || path.endsWith('.tgz')) {
-    return getRedirectedPackage(request, url, `https://${hostname}/${url.pathname}`, 0);
-  }
-
-  // For redirects we rewrite location to a proper domain
-  const init = {
-    cf: {
-      cacheTtlByStatus: {
-        "200-299": 300,
-        "301-302": 20,
-        "400-599": 10,
-      },
+  if (!obj) {
+    if (key === '' || key.endsWith('/')) { // object not found, append index.html and try again (like pages)
+      key += 'index.html';
+      obj = await getOrHead(key, { range, onlyIf });
+    } else { // object not found, redirect non-trailing slash to trailing slash (like pages) if index.html exists
+      key += '/index.html';
+      obj = await bucket.head(key);
+      if (obj) {
+        return permanentRedirect({ location: pathname + '/' });
+      }
     }
   }
 
-  // Fetch origin
-  let response = await fetch(changeUrl(request, url), init);
-  response = new Response(response.body, response);
-
-  // Follow original redirect for non-package requests
-  const location = response.headers.get('location');
-  const toReplace = domain + pathPrefix;
-  if (location && location.indexOf(toReplace) >= 0) {
-    response.headers.set('location', location.replace(toReplace, hostname));
+  if (obj && !key.endsWith("/")) {
+    // choose not to satisfy range requests for encoded content
+    // unfortunately we don't know it's encoded until after the first request
+    let disableRangeRequests = false;
+    console.log("obj", JSON.stringify(obj))
+    if (range && computeHeaders(obj, { range }).has('content-encoding')) {
+      console.log(`re-request without range`);
+      range = undefined;
+      obj = await bucket.get(key);
+      if (obj === null) throw new Error(`Object ${key} existed for .get with range, but not without`);
+      disableRangeRequests = true;
+    }
+    return computeObjResponse(obj, range ? 206 : 200, { range, onlyIf, disableRangeRequests });
   }
-  return response;
-}
 
-async function getRedirectedPackage(request: Request, url: URL, cacheKey: string, redirects: number):Promise<Response> {
-  // Jfrog put big files to S3 and redirects original requests there
-  // The 5 redirects is the maximum depth
-  let maxRedirects = 5;
-  const init = {
-    cf: {
-      cacheTtlByStatus: {
-        // Return files with 14d TTL
-        "200-299": 14 * 86400,
-        "301-302": 0,
-        "400-599": 10,
-      },
-      cacheKey: cacheKey,
-      cacheEverything: true,
-    },
-  };
-  let response = await fetch(changeUrl(request, url), init);
-  const location = response.headers.get('location');
-  if (location && redirects < maxRedirects) {
-    return getRedirectedPackage(request, new URL(location), cacheKey, redirects+1);
+
+  // R2 object not found, try listing a directory
+  let prefix = pathname.substring(1);
+  let redirect = false;
+  if (prefix !== '' && !prefix.endsWith('/')) {
+    prefix += '/';
+    redirect = true;
   }
-  return response;
+  const directoryListingLimitParam = searchParams.get('directoryListingLimit') || undefined;
+  const limit = 1000;
+  const options: R2ListOptions = { delimiter: '/', limit, prefix: prefix === '' ? undefined : prefix, cursor: searchParams.get('cursor') || undefined };
+  console.log(`list: ${JSON.stringify(options)}`);
+  const objects = await bucket.list(options);
+  if (objects.delimitedPrefixes.length > 0 || objects.objects.length > 0) {
+    const { cursor } = objects;
+    console.log({ numPrefixes: objects.delimitedPrefixes.length, numObjects: objects.objects.length, truncated: objects.truncated, cursor });
+    return redirect ? temporaryRedirect({ location: '/' + prefix }) : new Response(computeDirectoryListingHtml(objects, { prefix, cursor, directoryListingLimitParam }), { headers: { 'content-type': TEXT_HTML_UTF8 } });
+  }
+
+  return new Response(`Not found`, {
+    status: 404
+  })
 }
